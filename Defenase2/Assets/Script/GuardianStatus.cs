67d062d85c57f673b1ae71453de0c2792f84967b
maxwkssks <max0724_@naver.com>
2024-02-28 17:02:15 +0900

    init

---
+++ Defenase2/Assets/Script/GuardianStatus.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardianStatus : MonoBehaviour
+{
+    public int UpgradeCost { get; internal set; }
+    public int MaxTargetCount { get; internal set; }
+    public int Damage { get; internal set; }
+    public float AttackCycleTime { get; internal set; }
+    public float AttackRadius { get; internal set; }
+    public Color Color { get; internal set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
